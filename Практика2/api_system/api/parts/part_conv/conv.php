4a631ac623e16a72e30c493bc4ff004946684836
LicenDrinc <mihaoo4556@gmail.com>
2025-11-29 13:15:43 +0500

    practice 5

--- Практика2/api_system/api/parts/part_conv/conv.php
+++ Практика2/api_system/api/parts/part_conv/conv.php
@@ -1,81 +1,73 @@
 <?php
 
 # проверка наличия обязательных полей для этой команды
-$req = api_check_request(["apikey","file","filename","fileformat"]);
+$req = api_check_request(["apikey","file","filename","fileout"]);
 if (!$req) return true;
-list($apikey,$file,$filename,$fileformat) = $req;
+list($apikey,$file,$filename,$fileout) = $req;
 
 # проверка корректности ключа
 $userid = apikey_check($apikey);
 if (!$userid) return api_error(EFIELDBAD,"apikey");
 
-# создание временного файл: $n название файла, $d данные файла, возращает путь к файлу
-function creatingATemporaryFile($n, $d)
-{
-    $n = '/tmp/'.$n;
-    $filedata = base64_decode($d);
-    file_put_contents($n, $filedata);
+# создание временного файл:
+# $n название файла, $d данные файла, возращает путь к файлу
+function creatingATemporaryFile($n, $d) {
+    $n = '/tmp/'.md5(time().rand()).$n;
+    file_put_contents($n, base64_decode($d));
     return $n;
 }
 
-# создание пути или имяни файла: $f формат файла, $fN пути или имея файла, возращает путь к файлу
-function creatingAPathToConvertedFile($f ,$fN)
-{
-    $fNN = '';
-    for ($i = 0; $i <= strripos($fN,'.',-1); $i++) $fNN = $fNN.$fN[$i];
-    return $fNN.$f;
+# создание временных файлов
+$filenamelist = '';
+for ($i = 0; $i < count($filename); $i++) {
+    $filenametmp[$i] = creatingATemporaryFile($filename[$i], $file[$i]);
+    $filenamelist .= $filenametmp[$i].' '; # добавление в список файлов для конвертации
+    # проверка временого файла
+    if (!file_exists($filenametmp[$i])) {
+        $res['errors'] = "создание временого файла прошла не успешно";
+        $res['error'] = 900+$i;
+	for ($j = 0; $j < $i; $j++) unlink($filenametmp[$j]);
+	return api_json_result($res);
+    }
 }
 
-$filenametmp = $filename;
-# создание временного файл
-$filenametmp = creatingATemporaryFile($filenametmp, $file);
 # создание пути конвертированого файла
-$filenewnametmp = creatingAPathToConvertedFile($fileformat, $filenametmp);
-
-# проверка временого файла
-if (!file_exists($filenametmp))
-{
-    $res['errors'] = "файл не создался";
-    $res['error'] = 900;
-    return api_json_result($res);
-}
+$filenewnametmp = "/tmp/".md5(time().rand()).$fileout;
 
 # дабовление пораметры конвертации
-$porametrs = '';
-/*
-if ($fileformat == "pdf")
-{
-    if ($data['dpi'] != null)
-	$porametrs = $porametrs.'-density '.$data['dpi'].' ';
-    if ($data['compress'] != "none")
-	$porametrs = $porametrs.'-quality '.$data['qua'].' -compress '.$data['compress'].' ';
-    if ($data['mono'] == 'true')
-        $porametrs = $porametrs.'-monochrome ';
+$parametrs = '';
+$fileformat = substr($fileout,strrpos($fileout,'.')+1);
+$image = array('png','jpeg','jpg','gif','bmp','tiff','tif','webp','avif','heif','heic','raw','jxl','svg','eps','pdf','ai','cdr');
+$imageparaname = array('resize','quality','rotate','blur','bordercolor','border');
+$imagetopara = [
+    'resize'=>'-resize','quality'=>'-quality','rotate'=>'-rotate',
+    'blur'=>'-blur','bordercolor'=>'-bordercolor','border'=>'-border'
+];
+if (in_array($fileformat, $image)) {
+    for ($i = 0; $i < count($imageparaname); $i++) {
+        $reqpara = api_check_request($imageparaname[$i]);
+        if ($reqpara !== false) $parametrs .= $imagetopara[$imageparaname[$i]].' '.$reqpara.' ';
+    }
 }
-*/
 
 # конвертирование временого вайла
-$execlog = exec('convert '.$porametrs.$filenametmp.' '.$filenewnametmp);
-# удаление временого файла
-unlink($filenametmp);
+$execlog = exec('convert '.$filenamelist.$parametrs.$filenewnametmp);
 
-# создание название конвертированого файла для отправки
-$filenewname = creatingAPathToConvertedFile($fileformat,$filename);
+# удаление временых файлов
+for ($i = 0; $i < count($filenametmp); $i++) unlink($filenametmp[$i]);
 
 # проверка сканвертированого
-if (!file_exists($filenewnametmp))
-{
+if (!file_exists($filenewnametmp)) {
     $res['errors'] = "конвертация прошла не успешно";
-    $res['error'] = 800;
-    return api_json_result($res);
+    $res['error'] = 800; return api_json_result($res);
 }
 
+# полученое содержимого сконвертированого файла
 $newfile = base64_encode(file_get_contents($filenewnametmp));
-unlink($filenewnametmp);
+unlink($filenewnametmp); # удаление сконвертированого временого файла
 
 # все норм, утверждаем это
 $res['errors'] = ""; $res['error'] = 0;
-$res['file'] = $newfile;
-$res['filename'] = $filenewname;
+$res['file'] = $newfile; $res['filename'] = $fileout;
 
 return api_json_result($res);
--- Практика2/api_system/api/parts/part_conv/conv_test.php
+++ Практика2/api_system/api/parts/part_conv/conv_test.php
@@ -10,20 +10,49 @@ $address='http://localhost/api_system/api';
 
 $command='/part_conv/conv';
 
-$data = [
-//    "apikey" => $api_guest_key,
-    "apikey" => $api_super_key,
-    "filename" => substr($argv[1],strrpos($argv[1],'/')+1),
-    "file" => base64_encode(file_get_contents($argv[1])),
-    "fileformat" => $argv[2],
-];
+$i = 1;
+function casePara($n) {
+    global $i, $data, $argv;
+    $i++; $data[$n] = $argv[$i];
+}
 
+for ($j = 0; $i != $argc - 1 && strpos($argv[$i], '-') !== 0; $j++, $i++) {
+    $k = strrpos($argv[$i],'/');
+    if ($k === false) $data["filename"][$j] = $argv[$i];
+    else $data["filename"][$j] = substr($argv[$i], $k+1);
+    $data["file"][$j] = base64_encode(file_get_contents($argv[$i]));
+}
+
+while (true) {
+    if (strpos($argv[$i], '-') === 0) {
+	switch ($argv[$i]) {
+	    case "-resize"; casePara("resize"); break;
+	    case "-quality"; casePara("quality"); break;
+	    case "-rotate"; casePara("rotate"); break;
+	    case "-blur"; casePara("blur"); break;
+	    case "-bordercolor"; casePara("bordercolor"); break;
+	    case "-border"; casePara("border"); break;
+	    default; print($argv[$i]." нету того параметра\n");
+	}
+	$i++;
+    }
+    else break;
+}
+
+//$data["apikey"] = $api_guest_key;
+$data["apikey"] = $api_super_key;
+$k = strrpos($argv[$i],'/');
+if ($k === false) $data["fileout"] = $argv[$i];
+else $data["fileout"] = substr($argv[$i], $k+1);
 $result=edb_api_check($address,$command,$data);
+
+//var_dump($data);
+//var_dump($result);
 
 if ($result['error'] == 0)
 {
-    if (!file_put_contents(substr($argv[1],0,strrpos($argv[1],'/')+1).$result['filename'], base64_decode($result['file'])))
+    if (!file_put_contents(($k === false ? "" : substr($argv[$i],0,$k+1)).
+	$result['filename'], base64_decode($result['file'])))
 	var_dump($result);
 }
-else
-    var_dump($result);
+else var_dump($result);
